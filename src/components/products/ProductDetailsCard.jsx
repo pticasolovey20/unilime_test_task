9c1a5cf41127fe8ca5de8c0084f70b207c42e2e5
Dmytro Solovyov <dy.solovyov.work@gmail.com>
2024-02-24 03:24:20 +0200

    fixed product details card & update product item & updated proteted route

--- src/components/products/ProductDetailsCard.jsx
+++ src/components/products/ProductDetailsCard.jsx
@@ -1,6 +1,8 @@
+import { formatProductDate } from '../../utils/formatDate';
+
 import styles from './Products.module.scss';
 
-export const ProductDetailsCard = ({ price, thumbnail, title, body }) => {
+export const ProductDetailsCard = ({ price, thumbnail, title, body, created_at }) => {
 	return (
 		<div className={styles.card}>
 			<img src={thumbnail} alt={title} />
@@ -19,6 +21,11 @@ export const ProductDetailsCard = ({ price, thumbnail, title, body }) => {
 				<div className={styles.cardRow}>
 					<label>Body:</label>
 					<span>{body || '-'}</span>
+				</div>
+
+				<div className={styles.cardRow}>
+					<label>Date:</label>
+					<span>{formatProductDate(created_at) || '-'}</span>
 				</div>
 			</div>
 		</div>
